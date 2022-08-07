eeb60370b8939308ed82ad88c76de1e4f651e273
Fahad <92694740+iamfahad43@users.noreply.github.com>
2022-08-07 21:02:10 +0500

    Paraphraser API with parrot model
    please clone the parrot library from github -> https://github.com/PrithivirajDamodaran/Parrot_Paraphraser
    follow the guidance ->   https://github.com/protocolbuffers/protobuf/tree/main/python#installation
    Enjoy the API

---
+++ Paraphraser.py
@@ -0,0 +1,29 @@
+# API that rephrase your sentence
+
+# Para to Title Generator
+from parrot import Parrot
+import torch
+import warnings
+warnings.filterwarnings("ignore")
+
+# for reproduction the paraphrase
+def random_state(seed):
+  torch.manual_seed(seed)
+  if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(seed)
+
+random_state(1234)
+
+
+#Init models (make sure you init ONLY once if you integrate this to your code)
+parrot = Parrot(model_tag="prithivida/parrot_paraphraser_on_T5", use_gpu=False)
+
+phrases = [INPUT_TEXT]
+
+for phrase in phrases:
+  print("-"*100)
+  print("Input_phrase: ", phrase)
+  print("-"*100)
+  para_phrases = parrot.augment(input_phrase=phrase)
+  for para_phrase in para_phrases:
+   print(para_phrase)
